72f29ddff6f145df6d05de78c20b56bc9a356abc
Juliano Tavares <juliano.tavaress@gmail.com>
2023-01-26 15:00:13 -0300

    chore: update README

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-# expo-plugin-marketingcloudsdk
+# Expo-Plugin-Marketingcloudsdk
 
 "Plugin" designed to be compatible with the latest versions(8.0.+) of the Salesforce MarketingCloudSDK.
 
 It was developed as a form of study and research because the SDK does not have a plugin version (react-native-marketingcloudsdk), compatible with versions above 8.0.+.
 
-How to use:
+## Installation
+
+After cloning this project, use `yarn` or `npm` PackageManager to install:
+
+Using yarn:
+
+```bash
+yarn install
+```
+
+Using npm:
+
+```bash
+npm install
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate(not implemented yet).
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,18 +1,20 @@
-import fs from 'fs';
-import path from 'path';
-import { ConfigPlugin, createRunOncePlugin } from '@expo/config-plugins';
-import { MarketingCloudSdkPluginProps } from './types';
-import { withAndroidConfig } from './android';
-import { withIOSConfig } from './ios';
+import { ConfigPlugin, createRunOncePlugin } from "@expo/config-plugins";
+import fs from "fs";
+import path from "path";
+import { withAndroidConfig } from "./android";
+import { withIOSConfig } from "./ios";
+import { MarketingCloudSdkPluginProps } from "./types";
 
-const ERROR_PREFIX = 'Marketing Cloud SDK Plugin:';
+const ERROR_PREFIX = "Marketing Cloud SDK Plugin:";
 
-const withMarketingCloudSdk: ConfigPlugin<Partial<MarketingCloudSdkPluginProps> | undefined> = (config, props) => {
+const withMarketingCloudSdk: ConfigPlugin<
+  Partial<MarketingCloudSdkPluginProps> | undefined
+> = (config, props) => {
   if (!props) {
     throw new Error(`${ERROR_PREFIX} Must configure plugin options.`);
   }
 
-  const {serverUrl, appId, accessToken} = props;
+  const { serverUrl, appId, accessToken } = props;
 
   if (!serverUrl) {
     throw new Error(`${ERROR_PREFIX} Must provide server url.`);
@@ -24,10 +26,24 @@ const withMarketingCloudSdk: ConfigPlugin<Partial<MarketingCloudSdkPluginProps> 
     throw new Error(`${ERROR_PREFIX} Must provide access token.`);
   }
 
-  config = withAndroidConfig(config, {...props, serverUrl, appId, accessToken});
-  config = withIOSConfig(config, {...props, serverUrl, appId, accessToken});
+  config = withAndroidConfig(config, {
+    ...props,
+    serverUrl,
+    appId,
+    accessToken,
+  });
+  config = withIOSConfig(config, { ...props, serverUrl, appId, accessToken });
   return config;
 };
 
-const pkg = JSON.parse(fs.readFileSync(path.join(path.dirname(path.dirname(__dirname)), 'package.json'), 'utf8'));
-export default createRunOncePlugin(withMarketingCloudSdk, pkg.name, pkg.version);
+const pkg = JSON.parse(
+  fs.readFileSync(
+    path.join(path.dirname(path.dirname(__dirname)), "package.json"),
+    "utf8"
+  )
+);
+export default createRunOncePlugin(
+  withMarketingCloudSdk,
+  pkg.name,
+  pkg.version
+);
